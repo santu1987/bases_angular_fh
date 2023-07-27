353d2957f56498d3602e0c1bbf5d4502efbfa37a
santu1987 <gianni.d.santucci@gmail.com>
2023-07-27 12:13:16 -0400

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 export class AppComponent {
   public title: string = 'Hola Mundo';
   public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter() {
+    this.counter = 10;
+  }
 }
 
 bootstrapApplication(AppComponent);
