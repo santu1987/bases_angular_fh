81a998d9d0fbef40b326e635c165f346128be0fe
santu1987 <gianni.d.santucci@gmail.com>
2023-07-27 12:04:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# bases_angular_fh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-iy2sqt)
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  public title: string = 'Hola Mundo';
+  public counter: number = 10;
+}
+
+bootstrapApplication(AppComponent);
